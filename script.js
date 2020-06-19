27f070dcb67876f95169a905a49877befa391a08
Adam Black <sinegamy11@gmail.com>
2020-06-19 14:31:25 +0300

    add lesson07max

--- script.js
+++ script.js
@@ -1,54 +1,20 @@
 'use strict';
 
-const arrCreate = () => {
-	let arr = prompt('Введите 7 любых многозначных чисел через запятую', '');
-	if(arr) {
-		return arr.split(',');
-	} else {
-		return false;
+const weekWrap = document.querySelector('.week ul');
+const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sanday'];
+const date = new Date();
+
+week.forEach((day) => {
+	weekWrap.insertAdjacentHTML('beforeend', `<li>${day}</li>`);
+});
+
+const weekList = document.querySelectorAll('.week li');
+
+weekList.forEach((day, index) => {
+	if(day.textContent === 'saturday' || day.textContent === 'sanday') {
+		day.classList.add('weekend');
 	}
-}
-
-const isNumber = (item) => {
-	return !isNaN(parseFloat(item)) && isFinite(item);
-}
-
-const arrCheckItem = () => {
-	let arr = arrCreate();
-	let newArr = [];
-
-	for (let i = 0; i < arr.length; i++) {
-		if(!isNumber(arr[i])) {
-			console.log('work1');
-			return arrCheckItem();
-		} 
-		if(+arr[i][0] === 2 || +arr[i][0] === 4) {
-			console.log('work2');
-			newArr.push(arr[i]);
-		} 
-		if(i === (arr.length - 1)) {
-			console.log('work3');
-			return newArr;
-		}
-	};
-}
-console.log(arrCheckItem());
-
-
-const primeNumbersCreate = (start, finish) => {
-	let arr = [];
-	for(let startIteration = start + 1; startIteration <= finish; startIteration++) {
-		for(let i = 1; i <= startIteration; i++) {
-			if((startIteration % i === 0) && (i !== startIteration && i !== 1)) {
-				// если старт делится на какое то число помимо себя самого (старата) и 1, то цикл прекращается и продолжается верхний цикл, в котором старт увеличивается
-				break;
-			} else if(i === startIteration) {
-				arr.push([startIteration, `Делители этого числа: 1 и ${startIteration}`]);
-			}
-		}
+	if(index + 1 === date.getDay()) {
+		day.classList.add('this');
 	}
-	return arr;
-}
-
-const arr = primeNumbersCreate(1, 100);
-console.table(arr);
+});
