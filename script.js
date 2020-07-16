e8286231c0cbd2e8ab0a6fe17b72316b73eaaf59
Adam <sinegamy11@gmail.com>
2020-07-17 02:55:11 +0700

    fix

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 const color = document.querySelector('.collor'),
 			change = document.querySelector('.change');
 
-const arr16 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-
 const showColor = () => {
 	document.body.style.backgroundColor = color.textContent;
 	change.style.color = color.textContent;
